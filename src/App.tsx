535ba924ddb77198d97a32f0de22969aa1f4c3bb
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-24 20:28:48 +0900

    初期実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import React, { useState } from "react";
+import { css } from "@emotion/react";
 
-function App() {
+interface UserProfile {
+  name: string;
+  birthday: string;
+  phoneNumber: string;
+  photo: string;
+}
+
+const labelStyle = css`
+  margin-bottom: 5px;
+  font-weight: bold;
+`;
+
+const inputStyle = css`
+  margin: 10px 0;
+  display: block;
+`;
+
+const App = () => {
+  const [userProfile, setUserProfile] = useState<UserProfile>({
+    name: "",
+    birthday: "",
+    phoneNumber: "",
+    photo: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserProfile({ ...userProfile, [e.target.name]: e.target.value });
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const imageFile = e.target.files[0];
+      setUserProfile({ ...userProfile, photo: URL.createObjectURL(imageFile) });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label css={labelStyle} htmlFor="name">
+        名前
+      </label>
+      <input
+        css={inputStyle}
+        id="name"
+        type="text"
+        name="name"
+        onChange={handleInputChange}
+        placeholder="Name"
+      />
+
+      <label css={labelStyle} htmlFor="birthday">
+        誕生日
+      </label>
+      <input
+        css={inputStyle}
+        id="birthday"
+        type="date"
+        name="birthday"
+        onChange={handleInputChange}
+      />
+
+      <label css={labelStyle} htmlFor="phoneNumber">
+        電話番号
+      </label>
+      <input
+        css={inputStyle}
+        id="phoneNumber"
+        type="text"
+        name="phoneNumber"
+        onChange={handleInputChange}
+        placeholder="Phone Number"
+      />
+
+      <label css={labelStyle} htmlFor="photo">
+        プロフィール写真
+      </label>
+      <input
+        css={inputStyle}
+        id="photo"
+        type="file"
+        onChange={handleImageChange}
+      />
+
+      {userProfile.photo && <img src={userProfile.photo} alt="Profile" />}
+      {/* ここにダウンロード機能を実装 */}
     </div>
   );
-}
+};
 
 export default App;
